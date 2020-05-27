7e87a3e36664a76298ff9a23194ffee9621c0dda
vitortolentino <vitorafonso33@gmail.com>
2020-05-27 00:06:05 -0300

    fixing shell to correct instalation

--- startup.sh
+++ startup.sh
@@ -1,49 +1,63 @@
-echo 'initializing instalations'
-echo 'updating packages'
+clear
+yellow=`tput setaf 11`
+reset=`tput sgr0`
+
+echo "${yellow}Initializing instalations"
+echo "updating packages${reset}"
 sudo apt-get update
+clear
 
-echo 'installing curl' 
+echo "${yellow}Installing curl${reset}"
 sudo apt install curl -y
+clear
 
-echo 'installing git' 
+echo "${yellow}Installing zsh${reset}"
+sudo apt-get install zsh -y
+
+echo "${yellow}Installing Oh My Zsh${reset}"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+clear
+
+echo "${yellow}Installing git${reset}"
 sudo apt install git -y
+clear
 
-echo "What name do you want to use in GIT user.name?"
+echo "${yellow}What name do you want to use in GIT user.name?${reset}"
 read git_config_user_name
 git config --global user.name "$git_config_user_name"
 clear 
 
-echo "What email do you want to use in GIT user.email?"
+echo "${yellow}What email do you want to use in GIT user.email?${reset}"
 read git_config_user_email
 git config --global user.email $git_config_user_email
 clear
 
-echo 'installing zsh'
-sudo apt-get install zsh -y
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-chsh -s /bin/zsh
-
-echo 'installing vim'
+echo "${yellow}Installing vim${reset}"
 sudo apt install vim -y
 clear
 
-echo "Setting VIM as core editor"
+echo "${yellow}Setting VIM as core editor${reset}"
 git config --global core.editor vim
+clear
 
-echo 'installing tool to handle clipboard via CLI'
+echo "${yellow}Installing tool to handle clipboard via CLI${reset}"
 sudo apt-get install xclip -y
+clear
 
-echo "Generating a SSH Key"
-ssh-keygen -t rsa -b 4096 -C $git_config_user_email
+echo "${yellow}Generating a SSH Key${reset}"
+(cd ~/.ssh && ssh-keygen -t rsa -b 4096 -C $git_config_user_email)
+clear
 
+echo "${yellow}What name of genereted ssh?${reset}"
 read ssh_name
-ssh-add ~/.ssh/$ssh_name
-cat ~/.ssh/$ssh_name.pub | xclip -selection clipboard
+ssh-add "~/.ssh/$ssh_name"
+cat "~/.ssh/$ssh_name.pub" | xclip -selection clipboard
+echo "${yellow}ssh pub copied to clipboard${reset}"
 
-echo 'enabling workspaces for both screens' 
+echo "${yellow}Enabling workspaces for both screens${reset}"
 gsettings set org.gnome.mutter workspaces-only-on-primary false
 
-echo 'installing code'
+echo "${yellow}Installing Visual Studio Code${reset}"
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -51,7 +65,7 @@ sudo apt-get install apt-transport-https -y
 sudo apt-get update
 sudo apt-get install code -y # or code-insiders
 
-echo 'installing extensions'
+echo "${yellow}Installing extensions${reset}"
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension christian-kohler.path-intellisense
 code --install-extension dbaeumer.vscode-eslint
@@ -61,13 +75,15 @@ code --install-extension foxundermoon.shell-format
 code --install-extension pmneo.tsimporter
 code --install-extension waderyan.gitblame
 code --install-extension yzhang.markdown-all-in-one
+clear
 
-echo 'installing chrome' 
+echo "${yellow}Installing chrome${reset}"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+clear
 
-echo 'installing nvm' 
-sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash)"
+echo "${yellow}Installing nvm${reset}"
+sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | zsh)"
 
 export NVM_DIR="$HOME/.nvm" && (
 git clone https://github.com/creationix/nvm.git "$NVM_DIR"
@@ -77,35 +93,39 @@ git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+clear
 
 source ~/.zshrc
 nvm --version
 nvm install 13
 nvm alias default 13
-node --version
-npm --version
+clear
 
-echo 'installing autosuggestions' 
+echo "${yellow}installing autosuggestions${reset}"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshr
 source ~/.zshrc
+clear
 
-echo 'installing theme'
+echo "${yellow}installing theme${reset}"
 sudo apt install fonts-firacode -y
 wget -O ~/.oh-my-zsh/themes/node.zsh-theme https://raw.githubusercontent.com/skuridin/oh-my-zsh-node-theme/master/node.zsh-theme 
 sed -i 's/.*ZSH_THEME=.*/ZSH_THEME="node"/g' ~/.zshrc
+clear
 
-echo 'installing slack' 
+echo "${yellow}installing slack${reset}"
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
 sudo apt install ./slack-desktop-*.deb -y
+clear
 
-echo 'installing terminator'
+echo "${yellow}installing terminator${reset}"
 sudo apt-get update
 sudo apt-get install terminator -y
+clear
 
-echo 'adding dracula theme' 
+echo "${yellow}adding dracula theme${reset}"
 cat <<EOF >  ~/.config/terminator/config
-[global_config]
+[global_config]chr
   title_transmit_bg_color = "#ad7fa8"
 [keybindings]
   close_term = <Primary>w
@@ -136,7 +156,6 @@ cat <<EOF >  ~/.config/terminator/config
     cursor_color = "#aaaaaa"
 EOF
 
-
 cat <<EOF >>  ~/.config/terminator/config
 [[Dracula]]
     background_color = "#1e1f29"
@@ -148,8 +167,9 @@ cat <<EOF >>  ~/.config/terminator/config
     palette = "#000000:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#bbbbbb:#555555:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#ffffff"
     scrollback_infinite = True
 EOF
+clear
 
-echo 'installing docker' 
+echo "${yellow}installing docker${reset}"
 sudo apt-get remove docker docker-engine docker.io
 sudo apt install docker.io -y
 sudo systemctl start docker
@@ -158,29 +178,35 @@ docker --version
 
 chmod 777 /var/run/docker.sock
 docker run hello-world
+clear
 
-echo 'installing docker-compose' 
+echo "${yellow}installing docker-compose${reset}"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+clear
 
-echo 'installing dbeaver'
+echo "${yellow}installing dbeaver${reset}"
 wget -c https://dbeaver.io/files/6.0.0/dbeaver-ce_6.0.0_amd64.deb
 sudo dpkg -i dbeaver-ce_6.0.0_amd64.deb
 sudo apt-get install -f
+clear
 
-echo 'installing gnome twitch'
+echo "${yellow}installing gnome twitch${reset}"
 sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt update && sudo apt install gnome-twitch
+clear
 
-echo 'installing discord'
+echo "${yellow}installing discord${reset}"
 wget -O ~/Downloads/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 sudo dpkg -i ~/Downloads/discord.deb
+clear
 
-echo 'installing htop'
+echo "${yellow}installing htop${reset}"
 sudo apt-get install htop
+clear
 
-echo 'installing insomnia'
+echo "${yellow}installing insomnia${reset}"
 echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
     | sudo tee -a /etc/apt/sources.list.d/insomnia.list
 
@@ -189,9 +215,11 @@ wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
 
 sudo apt-get update
 sudo apt-get install insomnia
+clear
 
-echo 'installing MongoDB Compass'
+echo "${yellow}installing MongoDB Compass${reset}"
 wget -O ~/Downloads/compass.deb "https://downloads.mongodb.com/compass/mongodb-compass_1.21.2_amd64.deb"
 sudo dpkg -i ~/Downloads/compass.deb
+clear
 
-
+echo "${yellow}End of instalations${reset}"
